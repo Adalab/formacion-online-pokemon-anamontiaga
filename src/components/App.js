6aaf5d91ce86e52012918147f78326f0c4c0ae31
anamontiaga <amontiaga@gmail.com>
2019-10-17 17:04:06 +0200

    setstate with all data

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pokemones: [],
-      image: []
+      pokemones: []
     };
   }
 
@@ -17,30 +16,41 @@ class App extends React.Component {
 
   getPokemones() {
     fetchPokemones().then(data => {
-      this.setState({ pokemones: data.results });
+      for (let pokemon of data.results) {
+        fetch(pokemon.url)
+          .then(response => response.json())
+          .then(pokeInfo => {
+            const types = [];
+            for (let item of pokeInfo.types) {
+              types.push(item.type.name);
+            }
+            const infoPokemon = {
+              name: pokemon.name,
+              image: pokeInfo.sprites.front_default,
+              types: types
+            };
+            this.setState({ pokemones: [...this.state.pokemones, infoPokemon] });
+            // si pokemones está vacío, para qué repetirlo? ¿porque estamos metiendo un objeto dentro de un array?
+          });
+      }
     });
   }
 
   render() {
-    const { pokemones, image } = this.state;
+    const { pokemones } = this.state;
 
     return (
       <div className="app">
         <ul className="pokemones">
           {pokemones.map((pokemon, index) => {
-            fetch(pokemon.url)
-              .then(response => response.json())
-              .then(url => {
-                this.setState({ image: url.sprites.front_default });
-              });
-
             return (
-              <li className="pokemon" id={index} key={index}>
+              <li className="pokemon" id={index + 1} key={index + 1}>
                 <div className="card">
-                  <h2>{pokemon.name}</h2>
                   <div className="card__img--container">
-                    <img src={`${image}`} className="card__img" alt={pokemon.name}></img>
+                    <p className="id">ID /{index + 1} </p>
+                    <img src={pokemon.image} className="card__img" alt={pokemon.name}></img>
                   </div>
+                  <h2>{pokemon.name}</h2>
                 </div>
               </li>
             );
